716fbbb09cb83c6f73a75e1de62041a1c4b29317
ggvhcs <ggvhcs@gmail.com>
2025-02-06 11:17:35 -0500

    february 6, 2025.

--- components/01-HelloWorld-class.jsx
+++ components/01-HelloWorld-class.jsx
@@ -1,13 +1,22 @@
-/*** Hello World. ***/
+/*** Hello World Class. ***/
+import { Text, StyleSheet, View } from "react-native";
 import React, { Component } from "react";
-import { View, Text } from "react-native";
 
 export default class HelloWorldClass extends Component {
   render() {
     return (
-      <View>
-        <Text>!Hello World...Class Components.</Text>;
+      <View style={styles.container}>
+        <Text>!Hello World...Class Components.</Text>
       </View>
     );
   }
 }
+//
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
